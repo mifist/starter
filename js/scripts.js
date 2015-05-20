ba81c50cdb5356ce7991b7de7f63566c61430649
mifist <avanturka1992@gmail.com>
2015-05-20 18:36:08 +0300

    add new files

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Дарья on 16.05.2015.
+ */
+jQuery(function($){
+    $('.du-menu-toggle, .du-menu-overlay').click(function(){
+        //alert("Its alive!")
+        $('body').toggleClass('du-menu-open');
+    });
+});
+equalheight = function(container){
+
+    var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+    $(container).each(function() {
+
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
+}
+
+$(window).load(function() {
+    equalheight('.ba-grid .du-item');
+});
+
+
+$(window).resize(function(){
+    equalheight('.ba-grid .du-item');
+});
